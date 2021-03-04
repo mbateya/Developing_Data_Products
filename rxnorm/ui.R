dc031716579da089ed24bfa8bb798d6eb2136c6b
Mohammad Ateya <mbateya@gmail.com>
2021-03-03 21:14:21 -0500

    rxnorm update

---
+++ rxnorm/ui.R
@@ -0,0 +1,35 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Drug-Drug Interactions"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            textInput("Med1", "Med Name", value = "", width = '1000px', 
+                      placeholder = "Enter brand or generic name"),
+            textInput("Med2", "Med Name", value = "", width = '1000px', 
+                      placeholder = "Enter brand or generic name")
+        ),
+
+        # Show text of the RXCUI
+        mainPanel(
+            h4("Disclaimer from Source"),
+            textOutput("disclaimer"),
+            h3("Interactions:"),
+            textOutput("desc")
+        )
+    )
+))
