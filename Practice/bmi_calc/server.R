dcfc2e675bff396baf62ef74a94329ac5a031731
Mohammad Ateya <mbateya@gmail.com>
2021-03-17 18:00:43 -0400

    update

--- Practice/bmi_calc/server.R
+++ Practice/bmi_calc/server.R
@@ -1,3 +1,6 @@
+
+
+
 #
 # This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
@@ -12,20 +15,29 @@ library(tidyverse)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
     bmi <- reactive({
         wt <- input$weight
         ht <- input$height
-        round(wt/((ht/100)^2),1)
+        ifelse(is.null(wt)|is.null(ht),0,round(wt / ((ht / 100) ^ 2), 1))
     })
-    output$bmi <- renderText({bmi()})
+    output$bmi <-
+        renderText({
+            case_when(
+                bmi() > 0 ~ as.character(bmi()),
+                TRUE ~ ""
+            )
+        })
     
     output$category <- renderText({
-        case_when(bmi() > 30 ~ "obese"
-                  bmi() > 25 ~ "overweight"
-                  bmi() > 18 ~ "normal"
-                  TRUE ~ "underweight")
-
+        case_when(
+            is.null(bmi()) ~ "",
+            bmi() > 30 ~ "Obese",
+            bmi() > 25 ~ "Overweight",
+            bmi() > 18 ~ "Normal",
+            bmi() > 0 ~ "underweight",
+            TRUE ~ ""
+        )
+        
     })
-
+    
 })
--- Practice/bmi_calc/ui.R
+++ Practice/bmi_calc/ui.R
@@ -11,30 +11,62 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Body Mass Index (BMI) Calculator"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("weight",
-                        "Weight (Kg):",
-                        min = 1,
-                        max = 200,
-                        value = 70),
-            sliderInput("height",
-                        "Height (cm):",
-                        min = 1,
-                        max = 250,
-                        value = 170)
-            
-        ),
+  # Application title
+  titlePanel("Body Mass Index (BMI) Calculator"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(
+        "weight",
+        "Weight (Kg):",
+        min = 0,
+        max = 300,
+        step = 0.5,
+        value = NULL
+      ),
+      numericInput(
+        "height",
+        "Height (cm):",
+        min = 0,
+        max = 250,
+        step = 0.5,
+        value = NULL
+      )
+      ,
+      submitButton("Submit")
+      
+    ),
     
-        # Show a plot of the generated distribution
-        mainPanel(
-             textOutput("bmi"),
-             textOutput("category")
-        )
-    )
-))
+    # Show a plot of the generated distribution
+    mainPanel(
+      tags$body(
+        fluidRow(
+          h5("Body Mass Index (BMI)",style ="text-align:left;
+          color:black;
+          font-weight:bold" ),
+        column(
+          textOutput("bmi"),
+          width = 2,
+          style = "text-align:center;
+          color:blue;
+          font-weight:bold"
+        ))
+
+    ,
+    fluidRow(
+      br(),
+      column(
+          textOutput("category"),
+          width = 2,
+          style = "text-align:center;
+          color:crimson;
+          font-weight:bold"
+      )
+      ),
+    style = "background-color:powderblue"
+      )
+  )
+  )
+)
+)
